48d6c43b4e99ef2a04ca17048309dcde115db258
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-01-21 19:22:45 +0200

    refactor: instead passing setState variables from one component to another, I made functions for each state and passed the functions

--- client/src/components/Cards.tsx
+++ client/src/components/Cards.tsx
@@ -12,18 +12,40 @@ const Cards = () => {
   const [deliveryDate, setDeliveryDate] = useState(new Date());
   const [show, setShow] = useState(false);
 
+  const UpdateCartValue = (value: number) => {
+    setCartValue(value);
+  };
+  const UpdateDeliveryDistance = (value: number) => {
+    setDeliveryDistance(value);
+  };
+  const UpdateNumberOfItems = (value: number) => {
+    setNumberOfItems(value);
+  };
+  const UpdateDeliveryHour = (value: number) => {
+    setDeliveryHour(value);
+  };
+  const UpdateDeliveryMinute = (value: number) => {
+    setDeliveryMinute(value);
+  };
+  const UpdateDeliveryDate = (value: Date) => {
+    setDeliveryDate(value);
+  };
+  const UpdateShow = (value: boolean) => {
+    setShow(value);
+  };
+
   return (
     <div className="container d-flex justify-content-center">
       <div>
         {show === false ? (
           <Input
-            setCartValue={setCartValue}
-            setDeliveryDate={setDeliveryDate}
-            setDeliveryDistance={setDeliveryDistance}
-            setDeliveryHour={setDeliveryHour}
-            setDeliveryMinute={setDeliveryMinute}
-            setNumberOfItems={setNumberOfItems}
-            setShow={setShow}
+            setCartValue={UpdateCartValue}
+            setDeliveryDate={UpdateDeliveryDate}
+            setDeliveryDistance={UpdateDeliveryDistance}
+            setDeliveryHour={UpdateDeliveryHour}
+            setDeliveryMinute={UpdateDeliveryMinute}
+            setNumberOfItems={UpdateNumberOfItems}
+            setShow={UpdateShow}
           />
         ) : (
           <DeliveryFee
