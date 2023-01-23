598116de3e022a4fcdf4c6fb8a7ef935ae8ed840
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-01-23 19:20:10 +0200

    indentation fixed

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,13 +1,13 @@
 const Footer = () => {
-	return (
-		<footer className="mt-5">
-			<div className="bg-light fixed-bottom">
-				<p className="mt-2 text-sm-center font-weight-light">
-				© Anamaria Camarasan 2023
-				</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="mt-5">
+      <div className="bg-light fixed-bottom">
+        <p className="mt-2 text-sm-center font-weight-light">
+          © Anamaria Camarasan 2023
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -6,70 +6,70 @@ import DeliveryFee from "../components/DeliveryFee";
 import Footer from "../components/Footer";
 
 const Home = () => {
-	const [cartValue, setCartValue] = useState(0);
-	const [deliveryDistance, setDeliveryDistance] = useState(0);
-	const [numberOfItems, setNumberOfItems] = useState(0);
-	const [deliveryHour, setDeliveryHour] = useState(0);
-	const [deliveryMinute, setDeliveryMinute] = useState(0);
-	const [deliveryDate, setDeliveryDate] = useState(new Date());
-	const [show, setShow] = useState(false);
+  const [cartValue, setCartValue] = useState(0);
+  const [deliveryDistance, setDeliveryDistance] = useState(0);
+  const [numberOfItems, setNumberOfItems] = useState(0);
+  const [deliveryHour, setDeliveryHour] = useState(0);
+  const [deliveryMinute, setDeliveryMinute] = useState(0);
+  const [deliveryDate, setDeliveryDate] = useState(new Date());
+  const [show, setShow] = useState(false);
 
-	const UpdateCartValue = (value: number) => {
-		setCartValue(value);
-	};
-	const UpdateDeliveryDistance = (value: number) => {
-		setDeliveryDistance(value);
-	};
-	const UpdateNumberOfItems = (value: number) => {
-		setNumberOfItems(value);
-	};
-	const UpdateDeliveryHour = (value: number) => {
-		setDeliveryHour(value);
-	};
-	const UpdateDeliveryMinute = (value: number) => {
-		setDeliveryMinute(value);
-	};
-	const UpdateDeliveryDate = (value: Date) => {
-		setDeliveryDate(value);
-	};
-	const UpdateShow = (value: boolean) => {
-		setShow(value);
-	};
+  const UpdateCartValue = (value: number) => {
+    setCartValue(value);
+  };
+  const UpdateDeliveryDistance = (value: number) => {
+    setDeliveryDistance(value);
+  };
+  const UpdateNumberOfItems = (value: number) => {
+    setNumberOfItems(value);
+  };
+  const UpdateDeliveryHour = (value: number) => {
+    setDeliveryHour(value);
+  };
+  const UpdateDeliveryMinute = (value: number) => {
+    setDeliveryMinute(value);
+  };
+  const UpdateDeliveryDate = (value: Date) => {
+    setDeliveryDate(value);
+  };
+  const UpdateShow = (value: boolean) => {
+    setShow(value);
+  };
 
-	const fee = calculateFee({
-		cartValue,
-		deliveryDistance,
-		numberOfItems,
-		deliveryHour,
-		deliveryMinute,
-		deliveryDate,
-	});
+  const fee = calculateFee({
+    cartValue,
+    deliveryDistance,
+    numberOfItems,
+    deliveryHour,
+    deliveryMinute,
+    deliveryDate,
+  });
 
-	return (
-		<>
-			<div className="container d-flex justify-content-center">
-				<div>
-					{show === false ? (
-						<>
-							<Header />
-							<InputForm
-								setCartValue={UpdateCartValue}
-								setDeliveryDate={UpdateDeliveryDate}
-								setDeliveryDistance={UpdateDeliveryDistance}
-								setDeliveryHour={UpdateDeliveryHour}
-								setDeliveryMinute={UpdateDeliveryMinute}
-								setNumberOfItems={UpdateNumberOfItems}
-								setShow={UpdateShow}
-							/>
-						</>
-					) : (
-						<DeliveryFee deliveryFee={fee} />
-					)}
-				</div>
-			</div>
-			<Footer />
-		</>
-	);
+  return (
+    <>
+      <div className="container d-flex justify-content-center">
+        <div>
+          {show === false ? (
+            <>
+              <Header />
+              <InputForm
+                setCartValue={UpdateCartValue}
+                setDeliveryDate={UpdateDeliveryDate}
+                setDeliveryDistance={UpdateDeliveryDistance}
+                setDeliveryHour={UpdateDeliveryHour}
+                setDeliveryMinute={UpdateDeliveryMinute}
+                setNumberOfItems={UpdateNumberOfItems}
+                setShow={UpdateShow}
+              />
+            </>
+          ) : (
+            <DeliveryFee deliveryFee={fee} />
+          )}
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
