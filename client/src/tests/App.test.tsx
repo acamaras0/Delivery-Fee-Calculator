2b4a76e70e051a6b2500f5b254abb2c8744f833d
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-01-25 11:01:42 +0200

    tests for App

--- client/src/tests/App.test.tsx
+++ client/src/tests/App.test.tsx
@@ -1,15 +1,21 @@
-import { render } from "@testing-library/react";
-import App from "../App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
 
-describe("App", () => {
-  it("should render correctly", () => {
+describe('App', () => {
+  it('should render the App component with the className "App"', () => {
     const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
-  });
-
-  it("should display the logo", () => {
-    const { getByAltText } = render(<App />);
-    const logo = getByAltText("logo");
-    expect(logo).toBeInTheDocument();
+    const app = container.querySelector('.App');
+    expect(app).toBeInTheDocument();
   });
 });
+
+describe('App', () => {
+  it('should render the Home component inside the App component', () => {
+    const { container } = render(<App />);
+    const home = container.querySelector('.container');
+    expect(home).toBeInTheDocument();
+  });
+});
+
+
