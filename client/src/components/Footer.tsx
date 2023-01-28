153c56182d6374d30a4136e69490f58e96ba77dd
Anamaria Camarasan <anamaria.finn0@gmail.com>
2023-01-28 17:37:38 +0200

    footer changes

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,10 @@
 const Footer = () => {
   return (
-    <footer data-testid="footer" className="mt-5">
+    <footer data-testid="footer">
       <div className="bg-light fixed-bottom">
         <a href="https://github.com/acamaras0">
-          <p className="mt-2 text-sm-center font-weight-light">
-            © Anamaria Camarasan 2023
+          <p className="pt-2 text-sm font-weight-light">
+            Anamaria Camarasan 2023
           </p>
         </a>
       </div>
